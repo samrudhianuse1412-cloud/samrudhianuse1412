8069f22b1090f42c50fc21ec731a0e1d148bfe73
samrudhianuse1412-cloud <samrudhianuse1412@gmail.com>
2026-08-15 07:26:47 +0000

    positive.cpp

---
+++ positive.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+
+    cout << "Enter a number: ";
+    cin >> n;
+
+    if(n > 0)
+        cout << "Number is Positive";
+    else if(n < 0)
+        cout << "Number is Negative";
+    else
+        cout << "The number is Zero";
+
+    return 0;
+}
